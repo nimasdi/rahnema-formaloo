9973faf80d7ccfe1ec4f7c3f2bc155446d92c621
baharSaeedi <b.saeedi.ha@gmail.com>
2024-07-29 18:32:38 +0330

    add e2e test file for get result for one form

---
+++ Back-End/e2e/getFormsResult.spec.ts
@@ -0,0 +1,15 @@
+// import Request  from "supertest";
+// import { app } from "../src/app";
+// import { createGroupHelper } from "./function.hepler";
+
+describe("getFormResult",()=>{
+    it("should return list of forms with specific form id", async ()=>{
+        // const formId: number = 
+    })
+
+    it("should return 404 if form_id is not mongodb_id", async ()=>{
+    })
+
+    it("should return 404 if form_id is not exist", async ()=>{
+    })
+})
