3f123f1f2ef83b9f0fe829c6094c52baa33c2a1b
baharSaeedi <b.saeedi.ha@gmail.com>
2024-07-29 19:38:51 +0330

    add fill form test

---
+++ Back-End/e2e/fillTheForm.spec.ts
@@ -0,0 +1,60 @@
+import Request  from "supertest";
+import { app } from "../src/app";
+// import { createGroupHelper } from "./function.hepler";
+
+describe("getFormResult",()=>{
+
+    it("should return 200 if data is valid", async ()=>{
+
+        const filledForm = {
+            formId: "j23232nnmd32323",
+            answers: {
+                "name":"bahar",
+                "family":"haghighat",
+                "age":"25",
+                "phone":"09917780974"
+            }
+        }
+
+        await Request(app)
+        .post("/fillForm")
+        .send(filledForm)
+        .expect(200)
+
+    })
+
+    it("should return 403 if form_id is not mongodb_id", async ()=>{
+        const filledForm = {
+            formId: "2323233443",
+            answers: {
+                "name":"bahar",
+                "family":"haghighat",
+                "age":"25",
+                "phone":"09917780974"
+            }
+        }
+
+        await Request(app)
+        .post("/fillForm")
+        .send(filledForm)
+        .expect(403)
+    })
+
+    it("should return 404 if form_id is not exist", async ()=>{
+        const filledForm = {
+            formId: "j23232nnmd3s434342323",
+            answers: {
+                "name":"bahar",
+                "family":"haghighat",
+                "age":"25",
+                "phone":"09917780974"
+            }
+        }
+
+        await Request(app)
+        .post("/fillForm")
+        .send(filledForm)
+        .expect(404)
+        .expect({message: "form not found"})
+    })
+})
--- Back-End/e2e/getFormsResult.spec.ts
+++ Back-End/e2e/getFormsResult.spec.ts
@@ -6,8 +6,8 @@ describe("getFormResult",()=>{
     it("should return list of forms with specific form id", async ()=>{
         const formId: string = '1234567893477346';
 
-        const resultForms = await Request(app)
-        .post("/createExpense")
+        await Request(app)
+        .post("/getResult")
         .send({formId})
         .expect(200)
 
@@ -16,8 +16,8 @@ describe("getFormResult",()=>{
     it("should return 403 if form_id is not mongodb_id", async ()=>{
         const formId: string = '123456746';
 
-        const resultForms = await Request(app)
-        .post("/createExpense")
+        await Request(app)
+        .post("/getResult")
         .send({formId})
         .expect(403)
     })
@@ -25,8 +25,8 @@ describe("getFormResult",()=>{
     it("should return 404 if form_id is not exist", async ()=>{
         const formId: string = '123456746';
 
-        const resultForms = await Request(app)
-        .post("/createExpense")
+        await Request(app)
+        .post("/getResult")
         .send({formId})
         .expect(404)
         .expect({message: "form not found"})
