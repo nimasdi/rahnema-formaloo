8ed2e402783afea1e759afa6582008b0846c35f6
baharSaeedi <b.saeedi.ha@gmail.com>
2024-07-29 19:59:19 +0330

    app use fill form routes

--- Back-End/src/app.ts
+++ Back-End/src/app.ts
@@ -1,9 +1,12 @@
 import express from "express"
+import { fillFormRoutes } from "./routes/fillForm.route"
 
 export const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:true }))
+
+app.use("/fillForm",fillFormRoutes)
 
 app.use((req,res,next) => {
     res.status(404).send("Not Found!")
