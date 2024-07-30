0f91f15a83aaafa185f7af39caba2caadd087f62
baharSaeedi <b.saeedi.ha@gmail.com>
2024-07-30 23:22:09 +0330

    test

--- Back-End/e2e/fillTheForm.spec.ts
+++ Back-End/e2e/fillTheForm.spec.ts
@@ -1,13 +1,34 @@
 import Request  from "supertest";
-import { app } from "../src/app";
-// import { createGroupHelper } from "./function.hepler";
+import { Express } from "express";
+import { makeApp } from "../src/app";
+import MongoDBConnection from "../src/database/connect";
+import { FormRepository } from "../src/repositories/Form.repository";
+import { FormService } from "../src/services/Form.service";
+import Form from "../src/database/FormAnswers";
+import { seedForm } from "../src/database/seed.form";
 
 describe("getFormResult",()=>{
+
+    let app: Express;
+
+    beforeAll(async () => {
+
+        const formRepository = new FormRepository(Form)
+        const formService = new FormService(formRepository)
+        const uri = process.env.MONGO_URI || '';
+        const dbConnection = new MongoDBConnection(uri);
+
+        await dbConnection.connect().then(async () => {
+            await seedForm()
+            app = makeApp(formService)
+        }).catch(err => console.log("not connected to db"))
+
+    })
 
     it("should return 200 if data is valid", async ()=>{
 
         const filledForm = {
-            formId: "j23232nnmd32323",
+            formID: "66a93f98fc6427e8d2c5f28f",
             answers: {
                 "name":"bahar",
                 "family":"haghighat",
@@ -25,7 +46,7 @@ describe("getFormResult",()=>{
 
     it("should return 403 if form_id is not mongodb_id", async ()=>{
         const filledForm = {
-            formId: "2323233443",
+            formID: "2323233443",
             answers: {
                 "name":"bahar",
                 "family":"haghighat",
@@ -42,7 +63,7 @@ describe("getFormResult",()=>{
 
     it("should return 404 if form_id is not exist", async ()=>{
         const filledForm = {
-            formId: "j23232nnmd3s434342323",
+            formID: "j23232nnmd3s434342323",
             answers: {
                 "name":"bahar",
                 "family":"haghighat",
--- Back-End/e2e/getFormsResult.spec.ts
+++ Back-End/e2e/getFormsResult.spec.ts
@@ -1,8 +1,29 @@
 import Request  from "supertest";
-import { app } from "../src/app";
-// import { createGroupHelper } from "./function.hepler";
+import { Express } from "express";
+import { makeApp } from "../src/app";
+import MongoDBConnection from "../src/database/connect";
+import { FormRepository } from "../src/repositories/Form.repository";
+import { FormService } from "../src/services/Form.service";
+import Form from "../src/database/FormAnswers";
+import { seedForm } from "../src/database/seed.form";
 
 describe("getFormResult",()=>{
+    let app: Express;
+
+    beforeAll(async () => {
+
+        const formRepository = new FormRepository(Form)
+        const formService = new FormService(formRepository)
+        const uri = process.env.MONGO_URI || '';
+        const dbConnection = new MongoDBConnection(uri);
+
+        await dbConnection.connect().then(async () => {
+            await seedForm()
+            app = makeApp(formService)
+        }).catch(err => console.log("not connected to db"))
+
+    })
+
     it("should return list of forms with specific form id", async ()=>{
         const formId: string = '1234567893477346';
 
