e727da424e2ad07e2cbcce65c5b6354ef4f7edfe
Hosein Shahhoseini <hosein.shah.hoseni.mirash@gmail.com>
2024-08-01 18:42:56 +0330

    resolve merge conflict

--- Back-End/src/app.ts
+++ Back-End/src/app.ts
@@ -4,35 +4,33 @@ import { FillFormService } from "./services/FillForm.service"
 import { makeCreateFormRoute } from "./routes/create-form.route";
 import { makeUserFromRoute } from "./routes/getUserForms.route";
 import { FormService } from "./services/Form.service";
-import updateRoutes from './routes/form.route';
 
-export const makeApp = (fillFormService: FillFormService, formService: FormService) => {
+export const makeApp = (fillFormService: FillFormService , formService: FormService) => {
 
-  const app = express()
+    const app = express()
 
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
 
-  app.use("/fillForm", makeFillFormRoute(fillFormService))
-  app.use('/user', makeCreateFormRoute(formService))
-  app.use('/user', makeUserFromRoute(formService))
-  app.use('/update', updateRoutes);
+    app.use("/fillForm", makeFillFormRoute(fillFormService))
+    app.use('/user', makeCreateFormRoute(formService))
+    app.use('/user', makeUserFromRoute(formService))
 
-  const errorHandling: express.ErrorRequestHandler = (error, req, res, next) => {
+    const errorHandling: express.ErrorRequestHandler = (error, req, res, next) => {
 
-    if (error instanceof Error) {
-      res.status(400).json({ message: error });
+        if (error instanceof Error) {
+            res.status(400).json({ message: error });
+        }
+
+        res.status(500).send()
+
     }
 
-    res.status(500).send()
+    app.use(errorHandling)
 
-  }
+    app.use((req, res, next) => {
+        res.status(404).send("Not Found!")
+    })
 
-  app.use(errorHandling)
-
-  app.use((req, res, next) => {
-    res.status(404).send("Not Found!")
-  })
-
-  return app
+    return app
 }
