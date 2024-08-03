6773cdfa6853e11dfccca722977a254c238ac3a6
SeyedBardiaShams <bardiashams1375@gmail.com>
2024-08-03 14:04:42 +0330

    add cors

--- Back-End/src/app.ts
+++ Back-End/src/app.ts
@@ -1,55 +1,49 @@
-import express from "express";
-import { makeFillFormRoute } from "./routes/fillForm.route";
-import { FillFormService } from "./services/FillForm.service";
+import express from "express"
+import { makeFillFormRoute } from "./routes/fillForm.route"
+import { FillFormService } from "./services/FillForm.service"
 import { makeCreateFormRoute } from "./routes/create-form.route";
 import { makeUserFromRoute } from "./routes/getUserForms.route";
 import { FormService } from "./services/Form.service";
-import router from "./routes/form.route";
+import router from './routes/form.route'
 import { makeGetFormRoute } from "./routes/get-form.route";
 
-var cors = require("cors");
+var cors = require('cors')
 
-export const makeApp = (
-  fillFormService: FillFormService,
-  formService: FormService
-) => {
-  const app = express();
+export const makeApp = (fillFormService: FillFormService , formService: FormService) => {
 
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-    })
-  );
+    const app = express()
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+    app.use(cors({
+        origin: 'http://localhost:5173', 
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }));
 
-  app.use("/fillForm", makeFillFormRoute(fillFormService));
-  app.use("/user", makeCreateFormRoute(formService));
-  app.use("/user", makeUserFromRoute(formService));
-  app.use("/", router);
-  app.use("/", makeGetFormRoute(formService));
 
-  const errorHandling: express.ErrorRequestHandler = (
-    error,
-    req,
-    res,
-    next
-  ) => {
-    if (error instanceof Error) {
-      res.status(400).json({ message: error });
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+
+    app.use("/fillForm", makeFillFormRoute(fillFormService))
+    app.use('/user', makeCreateFormRoute(formService))
+    app.use('/user', makeUserFromRoute(formService))
+    app.use('/' , router)
+    app.use('/' , makeGetFormRoute(formService))
+
+    const errorHandling: express.ErrorRequestHandler = (error, req, res, next) => {
+
+        if (error instanceof Error) {
+            res.status(400).json({ message: error });
+        }
+
+        res.status(500).send()
+
     }
 
-    res.status(500).send();
-  };
+    app.use(errorHandling)
 
-  app.use(errorHandling);
+    app.use((req, res, next) => {
+        res.status(404).send("Not Found!")
+    })
 
-  app.use((req, res, next) => {
-    res.status(404).send("Not Found!");
-  });
-
-  return app;
-};
+    return app
+}
